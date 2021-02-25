5c3274b89bc109e4f8398fa5901296b7bcdc5ac5
karla-walker <indiablonix@gmail.com>
2021-02-25 06:48:08 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const meter = document.querySelector(".meter");
+const meterLength = meter.getTotalLength();
+const taskInput = document.querySelector(".task-input input");
+const addTaskBtn = document.querySelector(".task-input button");
+const taskName = document.querySelector(".task-name");
+const deleteTaskBtn = document.querySelector(".delete-task");
+const clearAllTasks = document.querySelector(".clear-button");
+const percentage = document.querySelector(".percentage");
+const completedTasks = document.querySelector(".tasks-complete");
+const totalTasks = document.querySelector(".tasks-total");
+
+// set meter dash array/offset
+meter.style.strokeDasharray = meterLength;
+meter.style.strokeDashoffset = meterLength;
+
+// create a new task
+
+// update number of tasks
+// update progress status %
+// update progress bar
+
+// change state of task when task is complete
+// update task text with dash across text
+// update number of tasks completed
+// update progress bar
+
+// delete task
+// clear all tasks
