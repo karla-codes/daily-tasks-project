932fa99c136d7fd7c3ed9bdd86a9b126ebad8c37
karla-walker <indiablonix@gmail.com>
2021-04-20 04:25:21 -0700

    Added function that updates the current total tasks and a function that updaates the total amount of completed tasks

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 const taskInput = document.querySelector('.task');
 const addTaskBtn = document.querySelector('#addTaskBtn');
 const taskList = document.querySelector('.task-list');
+let checked = 0;
 
 function createNewTask() {
   let taskText = taskInput.value;
@@ -20,13 +21,38 @@ function createNewTask() {
   appendChild(taskItem, button);
   appendChild(taskList, taskItem);
   taskInput.value = '';
+  updateTasksTotal();
 }
 
 function appendChild(parent, element) {
   parent.appendChild(element);
 }
 
+// when a new task is added, the task amount is updated
+function updateTasksTotal() {
+  const tasks = document.querySelectorAll('.task-item');
+  const totalTasks = document.querySelector('.tasks-total');
+  totalTasks.textContent = tasks.length;
+}
+
+// when a task is completed, the task completed amount is updated
+function updateTasksCompleted(target) {
+  const tasksComplete = document.querySelector('.tasks-complete');
+  if (target.checked) {
+    checked++;
+    tasksComplete.textContent = checked;
+  } else {
+    checked--;
+    tasksComplete.textContent = checked;
+  }
+}
+
 addTaskBtn.addEventListener('click', e => {
   e.preventDefault();
   createNewTask();
+});
+
+taskList.addEventListener('change', e => {
+  const currentTarget = e.target;
+  updateTasksCompleted(currentTarget);
 });
